03ff67eb20dd63c67d045f1ca0911bf13432c20f
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-08-12 12:47:18 +0300

    a

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VoidwareBackup/main/NewMainScript.lua", true))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VoidwareBakup/main/NewMainScript.lua", true))()
